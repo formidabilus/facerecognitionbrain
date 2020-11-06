6c6198428fc2213f954b250647b9ff5ca062f180
formidabilus <chiriac_razvan@ymail.com>
2020-11-06 22:56:30 +0200

    modified faceBoxes outside FaceRecognition

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,22 +1,22 @@
 import React from "react";
 import "./FaceRecognition.css";
 
-const FaceRecognition = ({ imageUrl, boxes }) => {
-  const faceBoxes = () =>
-    boxes.map((box) => {
-      return (
-        <div
-          className="bounding-box"
-          style={{
-            top: box.topRow,
-            right: box.rightCol,
-            bottom: box.bottomRow,
-            left: box.leftCol,
-          }}
-        ></div>
-      );
-    });
+const faceBoxes = ({ boxes }) =>
+  boxes.map((box) => {
+    return (
+      <div
+        className="bounding-box"
+        style={{
+          top: box.topRow,
+          right: box.rightCol,
+          bottom: box.bottomRow,
+          left: box.leftCol,
+        }}
+      ></div>
+    );
+  });
 
+const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
     <div className="center ma">
       <div className="absolute mt2">
